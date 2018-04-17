b925aff9e4be8e71686bdb11dc82f2d433c6a9e6
Jean-Francois Exbrayat <jexbraya@enrick.geos.ed.ac.uk>
2018-04-17 12:00:56 +0100

    Replaced Bloom's C code with latest version (will NOT be supported in the future)

---
+++ LIBRARY/CARDAMOM_C/models/DALEC_CDEA_CWD_LU_FIRES/src/DALEC_CDEA_CWD_LU_FIRES.c
@@ -0,0 +1,327 @@
+#pragma once
+#include <math.h>
+#include <stdio.h>
+
+// JFE 8 May 1027 - added a CWD pool to solve pb with tropical forests that accumulate a lot of C in soils
+
+double ACM(double const pars[], double const consts[])
+{
+  /*double gc=0,pp=0,qq=0,ci=0,e0=0,mult=0,dayl=0,cps=0,dec=0;*/
+  double gc,pp,qq,ci,e0,mult,dayl,cps,dec,GPP;
+  /*pars= &pars;  
+ *   consts= &consts;
+ *   */
+  gc=(double)pow(fabs(pars[8]),consts[9])/(consts[5] * pars[9] + 0.5 * ( pars[1]- pars[2]));
+  pp=(double)pars[0]*pars[3]/gc*consts[0]*exp(consts[7]*pars[1]);
+  qq=(double)consts[2]-consts[3];
+  ci=(double)0.5*(pars[4]+qq-pp+pow(pow(pars[4]+qq-pp,2)-4*(pars[4]*qq-pp*consts[2]),0.5));
+  e0=(double)consts[6]*pow(pars[0],2)/(pow(pars[0],2)+consts[8]);
+  dec=(double)-23.4*cos((360.*(pars[5]+10.)/365.)*pars[10]/180.)*pars[10]/180.;
+  mult=(double)tan(pars[6]*pars[10]/180)*tan(dec);
+  if (mult>=1){ 
+   dayl=24.;}  
+  else if(mult<=-1)
+  dayl=0.;
+  else{
+  dayl=(double)24.*acos(-mult) / pars[10];}
+
+
+  cps=(double)e0*pars[7]*gc*(pars[4]-ci)/(e0*pars[7]+gc*(pars[4]-ci));
+  GPP=cps*(consts[1]*dayl+consts[4]);
+  return GPP;
+}
+
+
+double offset(double const L, double const w) {
+
+/*solution to t = f*exp(-t^2)*/
+/*see dalecstepfunction2*/
+
+double mxc[7]={0.000023599784710, 0.000332730053021,    0.000901865258885,  -0.005437736864888,  -0.020836027517787,   0.126972018064287,   -0.188459767342504};
+
+double lf=log(L-1);
+double os=mxc[0]*pow(lf,6) + mxc[1]*pow(lf,5) + mxc[2]*pow(lf,4) + mxc[3]*pow(lf,3) + mxc[4]*pow(lf,2) + mxc[5]*lf +mxc[6];
+
+os=os*w;
+
+return os;
+}
+
+void CARBON_MODEL(double const *MET,double const *pars, double const deltat,int const nr, double const lat,
+double *LAI, double *NEE, double *FLUXES, double *POOLS) {
+
+
+    //printf("hello from DALEC_CDEA\n");
+    double gpppars[11],pi;
+    /*C-pools, fluxes, meteorology indices*/
+    int p,f,m,nxp, i;
+    int n=0;
+    pi=3.1415927;
+    double timestep; // JFE - to store timestep length
+    int nn;
+
+    /*constant gpppars terms*/
+    gpppars[3]=1;
+    gpppars[6]=lat;
+    gpppars[8]=-2.0;
+    gpppars[9]=1.0;
+    gpppars[10]=pi;
+
+    double constants[10]={pars[10],0.0156935,4.22273,208.868,0.0453194,0.37836,7.19298, 0.011136,2.1001,0.789798};
+
+    /*assigning values to pools*/
+    /*L,F,R,W,Lit,SOM,CWD*/
+    POOLS[0]=pars[17];
+    POOLS[1]=pars[18];
+    POOLS[2]=pars[19];
+    POOLS[3]=pars[20];
+    POOLS[4]=pars[21];
+    POOLS[5]=pars[22];
+    POOLS[6]=pars[23];
+
+    /*number of DALEC pools*/
+    int nopools=7;
+
+    /* NOTES FOR POOLS AND FLUXES
+    MET[:,0]: projday
+    MET[:,1]: mintemp
+    MET[:,2]: maxtemp
+    MET[:,3]: rad
+    MET[:,4]: co2
+    MET[:,5]: yearday
+
+    JFE added Jun 2014
+    MET[:,6]: removal
+    MET[:,7]: removal by fire (i.e. leading to emissions)
+    MET[:,8]: time-step length (in days)
+    
+    /*number of MET drivers*/
+    int nomet=9;
+     
+    /*fluxes - other*********
+    0.GPP
+    1.temprate
+    2.respiration_auto
+    3.leaf_production
+    4.labile_production
+    5.root_production
+    6.wood_production
+    7.labile_release
+    8.leaffall_factor
+    9.leaflitter_production
+    10.woodlitter_production  
+    11.rootlitter_production         
+ 	12.respiration_het_litter
+  	13.respiration_het_som
+  	14.litter2som
+  	15.labrelease_factor
+    16.cwd to litter - May 2017 - JFE
+
+    17.total release by fire - Jun 2014 / added by JFE 
+
+    18-24: release by fire for each pool
+    25-31: fluxes between pools
+
+    number of DALEC fluxes to store*/
+    int nofluxes=32;
+
+
+    /*constants for exponents of leaffall and labrelease factors*/
+    /*width*/
+    double wf=pars[15]*sqrt(2)/2;
+    double wl=pars[13]*sqrt(2)/2;
+
+    /*factor*/
+    double ff=(log(pars[4])-log(pars[4]-1))/2;
+    double fl=(log(1.001)-log(0.001))/2;
+
+    /*additional offset*/
+    double osf=offset(pars[4],wf);
+    double osl=offset(1.001,wl);
+
+    /*scaling to biyearly sine curve*/
+    double sf=365.25/pi;
+
+    // Fire specific variables - JFE
+    /*fully combusted fire flux (all pools except SOM)*/
+   // double CFF[5];
+
+    /*fully combusted fire flux (all pools except SOM)*/
+   // double NCFF[5];
+
+    /*combustion efficiencies*/
+    double CF[7] = {0.1,0.9,0.1,0.1,0.5,0.01,0.5};
+
+    /*resilience factor*/
+    double rfac=0.5;
+
+    /*repeating loop for each timestep*/
+    for ( n=0; n < nr; n++) {
+
+        /*ppol index*/
+        p=nopools*n;
+        /*next pool index*/
+        nxp=nopools*(n+1);
+        /*met index*/
+        m=nomet*n;
+        /*flux array index*/
+        f=nofluxes*n;
+       
+        /*LAI*/
+        LAI[n]=POOLS[p+1]/pars[16]; 
+  
+        /*GPP*/
+        gpppars[0]=LAI[n];
+        gpppars[1]=MET[m+2];
+        gpppars[2]=MET[m+1];
+        gpppars[4]=MET[m+4];
+        gpppars[5]=MET[m+5];
+        gpppars[7]=MET[m+3];
+
+        /*GPP - daily timestep*/
+        FLUXES[f+0]=ACM(gpppars,constants);
+        /*temprate - now comparable to Q10 - factor at 0C is 1*/
+        FLUXES[f+1]=exp(pars[9]*0.5*(MET[m+2]+MET[m+1]));
+        /*respiration auto*/
+        FLUXES[f+2]=pars[1]*FLUXES[f+0];
+        /*leaf production rate*/
+        FLUXES[f+3]=(FLUXES[f+0]-FLUXES[f+2])*pars[2];
+        /*labile production*/
+        FLUXES[f+4] = (FLUXES[f+0]-FLUXES[f+2]-FLUXES[f+3])*pars[13-1];              
+
+        /*root production*/        
+        FLUXES[f+5] = (FLUXES[f+0]-FLUXES[f+2]-FLUXES[f+3]-FLUXES[f+4])*pars[4-1];            
+
+        /*wood production rate*/       
+        FLUXES[f+6] = FLUXES[f+0]-FLUXES[f+2]-FLUXES[f+3]-FLUXES[f+5]-FLUXES[f+4]; 
+
+
+        /*leaf fall factor*/
+        /*FLUXES[f+8]=pow((0.75+cos((MET[m+5]-pars[15-1])*(2*pi/365.25))/4),10)*pars[5-1];*/
+        /*FLUXES[f+8]=1- pow((1 +0.00001 - 1/pars[4]),nf*exp(nf*sin((MET[m+0]-pars[14])/sf)*sf)/pow(1+exp(nf*sin((MET[m+0]-pars[14])/sf)*sf),2));*/
+        FLUXES[f+8] = (2/sqrt(pi))*(ff/wf)*exp(-pow(sin((MET[m+0]-pars[14]+osf)/sf)*sf/wf,2));
+
+
+        /*Labrelease factor*/
+        FLUXES[f+15]=(2/sqrt(pi))*(fl/wl)*exp(-pow(sin((MET[m+0]-pars[11]+osl)/sf)*sf/wl,2));
+         
+        // JFE - replaced constant deltat by dynamic timestep
+        timestep=MET[m+8];
+       // printf("Adapting timestep %f", timestep);
+
+        /*total labile release - mean at daily timestep */
+        //FLUXES[f+7]=POOLS[p+0]*(1-pow(1-FLUXES[f+15],deltat))/deltat;
+        FLUXES[f+7]=POOLS[p+0]*(1-pow(1-FLUXES[f+15],timestep))/timestep;             
+      
+        /*total leaf litter production*/       
+        //FLUXES[f+9] = POOLS[p+1]*(1-pow(1-FLUXES[f+8],deltat))/deltat;                     
+        FLUXES[f+9] = POOLS[p+1]*(1-pow(1-FLUXES[f+8],timestep))/timestep;
+ 
+        // total wood litter production aka wood to cwd  JFE - MAY 17
+        //FLUXES[f+10] = POOLS[p+3]*(1-pow(1-pars[6-1],deltat))/deltat;                    
+        FLUXES[f+10] = POOLS[p+3]*(1-pow(1-pars[6-1],timestep))/timestep;    
+
+        /*root litter production*/
+        //FLUXES[f+11] = POOLS[p+2]*(1-pow(1-pars[7-1],deltat))/deltat;                    
+        FLUXES[f+11] = POOLS[p+2]*(1-pow(1-pars[7-1],timestep))/timestep;                                    
+
+        /*respiration heterotrophic litter*/
+        //FLUXES[f+12] = POOLS[p+4]*(1-pow(1-FLUXES[f+1]*pars[8-1],deltat))/deltat;
+        FLUXES[f+12] = POOLS[p+4]*(1-pow(1-FLUXES[f+1]*pars[8-1],timestep))/timestep;
+                
+        /*respiration heterotrophic SOM*/
+        //FLUXES[f+13] = POOLS[p+5]*(1-pow(1-FLUXES[f+1]*pars[9-1],deltat))/deltat;
+        FLUXES[f+13] = POOLS[p+5]*(1-pow(1-FLUXES[f+1]*pars[9-1],timestep))/timestep;                  
+
+        /*litter to SOM*/
+        //FLUXES[f+14] = POOLS[p+4]*(1-pow(1-pars[1-1]*FLUXES[f+1],deltat))/deltat; 
+        FLUXES[f+14] = POOLS[p+4]*(1-pow(1-pars[1-1]*FLUXES[f+1],timestep))/timestep; 
+       
+        // cwd to litter
+        FLUXES[f+16] = POOLS[p+6]*(1-pow(1-FLUXES[f+1]*pars[24],timestep))/timestep;         
+
+        /* update pools */
+
+        // labile pool
+        //POOLS[nxp+0] = POOLS[p+0] + (FLUXES[f+4]-FLUXES[f+7])*deltat;
+        POOLS[nxp+0] = POOLS[p+0] + (FLUXES[f+4]-FLUXES[f+7])*timestep;
+
+        // foliar pool
+        //POOLS[nxp+1] =  POOLS[p+1] + (FLUXES[f+3] - FLUXES[f+9] + FLUXES[f+7])*deltat;
+        POOLS[nxp+1] =  POOLS[p+1] + (FLUXES[f+3] - FLUXES[f+9] + FLUXES[f+7])*timestep;
+
+        // wood pool
+        //POOLS[nxp+3] = POOLS[p+3] +  (FLUXES[f+6] - FLUXES[f+10])*deltat;
+        POOLS[nxp+3] = POOLS[p+3] +  (FLUXES[f+6] - FLUXES[f+10])*timestep;
+
+        // root pool
+        //POOLS[nxp+2] = POOLS[p+2] + (FLUXES[f+5] - FLUXES[f+11])*deltat;
+        POOLS[nxp+2] = POOLS[p+2] + (FLUXES[f+5] - FLUXES[f+11])*timestep;
+
+        // litter pool
+        //POOLS[nxp+4] = POOLS[p+4] + (FLUXES[f+9] + FLUXES[f+11] - FLUXES[f+12] - FLUXES[f+14])*deltat;
+        POOLS[nxp+4] = POOLS[p+4] + (FLUXES[f+9] + FLUXES[f+11] + FLUXES[f+16] - FLUXES[f+12] - FLUXES[f+14])*timestep;                
+
+        // SOM pool
+        //POOLS[nxp+5]= POOLS[p+5]+ (FLUXES[f+14] - FLUXES[f+13]+FLUXES[f+10])*deltat;
+        POOLS[nxp+5]= POOLS[p+5]+ (FLUXES[f+14] - FLUXES[f+13])*timestep;
+
+        // CWD pool - add wood litter prod and remove transfer to litter
+        POOLS[nxp+6] = POOLS[p+6] + (FLUXES[f+10]-FLUXES[f+16])*timestep;
+
+        NEE[n]=-FLUXES[f+0]+FLUXES[f+2]+FLUXES[f+12]+FLUXES[f+13];
+
+        /* perform the deforestation removal bit if required */
+        if (MET[m+6] > 0.) 
+        {
+         //   
+            POOLS[nxp+0] = POOLS[nxp+0]*(1-MET[m+6]);
+            POOLS[nxp+1] = POOLS[nxp+1]*(1-MET[m+6]);
+            POOLS[nxp+3] = POOLS[nxp+3]*(1-MET[m+6]);
+      //      printf("End deforestation %03i\n %f\n", n, POOLS[nxp+0]);
+        } // end removal
+
+        // perform the fire part if required
+        FLUXES[f+17]=0.;
+        // perform the fire part if required
+        if (MET[m+7] > 0.) 
+        {
+            /*first fluxes*/
+         //   printf("Fire detected\n %f\n", POOLS[nxp+0]);
+	        /*LABILE*/
+        		/*Calculating all fire transfers (1. combustion, and 2. litter transfer)*/
+	            /*note: all fluxes are in gC m-2 day-1*/
+	        for (nn=0;nn<7;nn++){FLUXES[f+18+nn] = POOLS[nxp+nn]*MET[m+7]*CF[nn]/timestep;}
+	        for (nn=0;nn<7;nn++){FLUXES[f+25+nn] = POOLS[nxp+nn]*MET[m+7]*(1-CF[nn])*(1-rfac)/timestep;}
+            //no litter transfer from soil
+            FLUXES[f+25+5] = 0;
+
+	        /*Adding all fire pool transfers here*/
+	        /*live C pools*/	
+	        for (nn=0;nn<4;nn++){POOLS[nxp+nn]=POOLS[nxp+nn]-(FLUXES[f+18+nn]+FLUXES[f+25+nn])*timestep;}
+	        /*dead C pools*/
+	        //litter: input from labile,foliar,root, output to soil
+	        POOLS[nxp+4]=POOLS[nxp+4]+(FLUXES[f+25]+FLUXES[f+25+1]+FLUXES[f+25+2]-FLUXES[f+18+4]-FLUXES[f+25+4])*timestep;
+	        // som: input from wood litter cwd
+	        POOLS[nxp+5]=POOLS[nxp+5]+(FLUXES[f+25+3]+FLUXES[f+25+4]+FLUXES[f+25+6]-FLUXES[f+18+5])*timestep;
+            //cwd: output to som
+            POOLS[nxp+6]=POOLS[nxp+6]+(-FLUXES[f+18+6]-FLUXES[f+25+6])*timestep;
+
+	        /*fires - total flux in gC m-2 day-1*/
+	        /*this term is now (essentially) obsolete*/
+	        /*replace in next version of DALEC_FIRES*/
+	        for (nn=0;nn<7;nn++){FLUXES[f+17]+=FLUXES[f+18+nn];}
+
+	        /*Net ecosystem exchange*/
+	        /*WARNING WARNING WARNING*/
+	        /*NEE should not contain fires (or should it?)*/
+	        /*Always check!!!*/
+        } // end fire   // end fire  
+ 
+    } // end daily loop
+
+} // end DALEC_CDEA_LU
+
+
+
+
---
+++ LIBRARY/CARDAMOM_C/models/DALEC_CDEA_LU_FIRES_ET/likelihood/EDCs.c
@@ -0,0 +1,456 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../../general/cardamom_structures.c"
+
+struct EDCDIAGNOSTIC
+{
+    int EDC;
+    int DIAG;
+    /*allocating space for 100 checks (in case future additions are made, etc.).*/
+    int PASSFAIL[100];
+    int nedc;
+};
+
+int EDC1_LU_FIRES_ET(double const pars[], struct EDCDIAGNOSTIC *EDCD, double meantemp, double meanrad)
+{
+    // ALL EDCs set as 1 initially
+    EDCD->nedc=100;
+    int n; for (n=0;n<EDCD->nedc;n++){EDCD->PASSFAIL[n]=1;}
+
+    // declaring variables and constants for subsequent EDCs
+    int EDC=1;
+    int DIAG=EDCD->DIAG;
+    double const fauto=pars[1];
+    double const ffol=(1-fauto)*pars[2];
+    double const flab=(1-fauto-ffol)*pars[12];
+    double const froot=(1-fauto-ffol-flab)*pars[3];
+    double const fwood=1-fauto-ffol-flab-froot;
+
+    // added constant values for allocations wrt NPP
+    double const ffol_npp = ffol/(1-fauto);
+    double const flab_npp = flab/(1-fauto);
+    double const froot_npp = froot/(1-fauto);
+    double const fwood_npp = fwood/(1-fauto);
+    /*fraction of GPP som under equilibrium conditions*/
+    //double const fsom=fwood+(froot+flab+ffol)*pars[0]/(pars[0]+pars[7]);
+ 
+    /*  EDC CHECK NO 1
+        TOR_LIT faster than TOR_SOM */
+    if ((EDC==1 || DIAG==1) && (pars[8]>pars[7])){EDC=0;EDCD->PASSFAIL[0]=0;}
+
+    /*  EDC CHECK NO 2
+        Litter2SOM greater than SOM to Atm. rate */
+    if ((EDC==1 || DIAG==1) && (pars[0]<pars[8])){EDC=0;EDCD->PASSFAIL[1]=0;}
+
+    /*  EDC CHECK NO 3
+        TOR_FOL faster than TOR_WOOD */
+    if ((EDC==1 || DIAG==1) && (pars[5]>pars[4])){EDC=0;EDCD->PASSFAIL[2]=0;}
+
+    /*  EDC CHECK NO 4
+        Root turnover greater than SOM turnover at meantemp */
+    if ((EDC==1 || DIAG==1) && (pars[6]<pars[8]*exp(pars[9]*meantemp))){EDC=0;EDCD->PASSFAIL[3]=0;}
+
+    /*  EDC CHECK NO 5 */
+    if ((EDC==1 || DIAG==1) && (((ffol+flab)>5*froot) || ((ffol+flab)*5<froot))){EDC=0;EDCD->PASSFAIL[4]=0;}
+
+    // check actual allocation fraction -flab,ffol,froot,fwood in that order: from EDC ID 40 onwards
+    //amended to use fnpp wrt discussion w Luke and Malhi et al 10.1098/rstb.2011.0062 10.1111/nph.14189
+//    if ((EDC==1 || DIAG==1) && ((flab<0.01) || (flab > 0.9))){EDC=0;EDCD->PASSFAIL[40]=0;}
+//    if ((EDC==1 || DIAG==1) && ((ffol<0.01) || (ffol > 0.9))){EDC=0;EDCD->PASSFAIL[41]=0;}
+//    if ((EDC==1 || DIAG==1) && ((froot<0.01) || (froot > 0.9))){EDC=0;EDCD->PASSFAIL[42]=0;}
+//    if ((EDC==1 || DIAG==1) && ((fwood<0.01) || (fwood > 0.9))){EDC=0;EDCD->PASSFAIL[43]=0;}
+
+//    if ((EDC==1 || DIAG==1) && ((flab<0.01) || (flab > 0.9))){EDC=0;EDCD->PASSFAIL[40]=0;}
+    if ((EDC==1 || DIAG==1) && (ffol_npp<0.10)){EDC=0;EDCD->PASSFAIL[41]=0;}
+    if ((EDC==1 || DIAG==1) && (froot_npp<0.05)){EDC=0;EDCD->PASSFAIL[42]=0;}
+    if ((EDC==1 || DIAG==1) && (fwood_npp>0.6)){EDC=0;EDCD->PASSFAIL[43]=0;}
+//    if ((EDC==1 || DIAG==1) && ((fwood<0.01) || (fwood > 0.6))){EDC=0;EDCD->PASSFAIL[43]=0;}
+    
+
+    /*  GSI specific checks
+        Tmn,min < Tmn,max */
+    //if ((EDC==1 || DIAG==1) && (pars[13]>pars[14])){EDC=0;EDCD->PASSFAIL[5]=0;}
+
+    //  Photoperiod,min < Photoperiod,max
+    //if ((EDC==1 || DIAG==1) && (pars[15]>pars[23])){EDC=0;EDCD->PASSFAIL[6]=0;}
+
+    //  VPD,min < VPD,max
+    // if ((EDC==1 || DIAG==1) && (pars[24]>pars[25])){EDC=0;EDCD->PASSFAIL[7]=0;}
+
+ //   for (n=0;n<5;n++) {if (EDCD->PASSFAIL[n]==0) {printf("fail %d\n", n);}}
+
+
+    return EDC;
+}
+
+
+double mean_pool(double *PA, int p, int nc, int nopools)
+{
+    // variable declarations
+    int c;
+    double meanpool=0;
+    // deriving mean of pool p
+    for (c=0;c<nc;c++) {meanpool=meanpool+PA[c*nopools+p]/(double)nc;}
+    
+    return meanpool;
+}
+
+double mean_annual_pool(double *POOLS, int year, int pool, int nopools,int deltat){
+/*inputs
+ * POOLS: Pools double pointer, as output from DALEC
+ * year: year for which to average (first year = 0)
+ * pool: the specific pool 
+ * nc
+declarations*/
+int c;
+double meanpool=0;
+// deriving mean of pool p
+int stday=floor(365.25*year/deltat);
+int enday=floor(365.25*(year+1)/deltat);
+for (c=stday;c<enday;c++){
+meanpool=meanpool+POOLS[c*nopools+pool]/(enday-stday);}
+// returing meanpool value*/
+return meanpool;}
+
+
+double expdecay2(double const *POOLS, int pool, int nodays,int deltat,int nopools)
+/*only accepting pool and number of days, the rest done here*/
+{
+/*using 365 day averaging window for each year!*/
+
+/*explicitly calculating number of years*/
+//int noyears=floor(nodays*deltat/365);
+int n;
+
+/*yearly means and yearly means with offset Ds = 100 days*/
+/*Deriving exponential decay coefficients a,b and c in equation
+ * Cexp = a + b*exp(c*t)*/
+/*four mean pool values to be derived*/
+/* MP0 = mean pool (year 1 to year end-2)
+ * MP1 = mean pool (year 2 to year end-1)
+ * MP0os = mean pool (year 1+os to year end-2+os)
+ * MP1os = mean pool (year 1+os to year end-2+os)*/
+double MP0=0,MP1=0,MP0os=0,MP1os=0;
+/*averaging window*/
+int aw=floor(365./(double)deltat);
+/*offset in days*/
+/*OFFSET = 1 is ideal to capture rapid exponential decay without compromising quality of fit*/
+int os=1;
+/*deriving coefficients to *
+ * in Cexp = A + B exp(C*t);*/
+//double A,B,b,C;
+
+/*mean pool within each averaging window*/
+for (n=0;n<aw;n++){MP0=MP0+POOLS[n*nopools+pool];};MP0=MP0/(double)aw;
+for (n=aw;n<aw*2;n++){MP1=MP1+POOLS[n*nopools+pool];};MP1=MP1/(double)aw;
+for (n=os;n<aw+os;n++){MP0os=MP0os+POOLS[n*nopools+pool];};MP0os=MP0os/(double)aw;
+for (n=aw+os;n<aw*2+os;n++){MP1os=MP1os+POOLS[n*nopools+pool];};MP1os=MP1os/(double)aw;
+
+/*deriving mean gradient ratio dcdt1/dcdt0*/
+/*dcdt1 is the numeric gradient between n+1 and n+365+1*/
+/*dcdt0 is the numeric gradient between n and n+365*/
+//double dcdtr=0,dcdt1,dcdt0;
+
+double dcdty1=(MP1os-MP0os);
+double dcdty0=(MP1-MP0);
+
+/*denominators*/
+
+/*using multiyear mean to determine c*/
+double C=log(dcdty1/dcdty0)/((double)os*deltat);
+/*deriving final exp. decay fit with startpoint = startpoint of pool*/
+
+/*compared and validated against dalec_expdecay3.m*/
+/*
+printf("noyears = %d\n",noyears);
+printf("MP0 = %f\n",MP0);
+printf("MP1 = %f\n",MP1);
+printf("MP0os = %f\n",MP0os);
+printf("MP1os = %f\n",MP1os);
+printf("A = %e\n",A);
+printf("B = %e\n",B);
+printf("C = %e\n",C);
+printf("aw = %d\n",aw);
+printf("dcdty1 = %f\n",dcdt1);
+printf("dcdty0 = %f\n",dcdt0);
+*/
+
+/*half life must be more than noyears*/
+
+/*if (fabs(-log(2)/C)<noyears*365.25 && C<0 && finite(C) && abs(B)>abs(A)*0.01){EDC=0;}*/
+
+
+return C;
+
+
+}
+
+
+int EDC2_LU_FIRES_ET(double const pars[], double *MET, double *LAI, double *NEE, double *POOLS,double *FLUXES, PARAMETER_INFO PI, DATA D, struct EDCDIAGNOSTIC *EDCD, double meantemp)
+{
+
+// THESE EDC2 checks are for DALEC_CDEA
+int EDC=1,n=0;
+int DIAG=EDCD->DIAG; // 1
+
+int nomet=D.nomet, nopools=D.nopools; 
+int nofluxes=D.nofluxes, nodays = D.nodays; 
+
+int deltat=(int)(MET[nomet]-MET[0]);
+
+// deriving mean pools here
+double* MPOOLS;
+MPOOLS=malloc(nopools*sizeof(double));
+for (n=0;n<nopools;n++){MPOOLS[n]=mean_pool(POOLS,n,D.nodays+1,nopools);};
+
+
+/***********************EDCs start here****************************/
+
+
+
+/*EDC LAI related:*/
+/*now obsolete*/
+/*MAX(Cfol)/clma <LAImax*/
+/*double LAImax=10;
+if (EDC==1 || DIAG==1){
+for (n=0;n<nodays;n++){
+if (LAI[n]>LAImax){EDC=0;EDCD->PASSFAIL[5-1]=0;}}}*/
+/*MLAI=mean_pointer_vector(LAI,nodays);*/
+
+
+
+/*EDC no 9*/
+/*0.2*Cf < Cr < 5*Cf*/
+/*Cfoliar : Croot = 5:1 or 1:5*/
+if ((EDC==1 || DIAG==1) && (MPOOLS[1]>MPOOLS[2]*5 || MPOOLS[1]*5<MPOOLS[2])){EDC=0;EDCD->PASSFAIL[8]=0;}// printf("failed no. 8");}
+
+
+/*EDC 10
+ *G10 = 2 - growth factor in 10 years
+ *for any further constraint please incorporate in likelyhood*/
+int noyears=floor(nodays*deltat/365); if (noyears == 0) {noyears = 1;}
+double G=0.1;
+int y;
+
+
+
+//printf("No days %i\n", nodays);
+//printf("Deltat %i\n", deltat);
+//printf("No years %i\n", noyears);
+/*mean annual pool array*/
+
+double *MAPOOL=malloc(noyears*sizeof(double));
+for (n=0;n<nopools;n++){
+/*Rapid POOL Growth: cannot increase by more than factor of Gn over N yrs*/
+/*note - this is GROWTH ONLY!!!*/
+
+// step 1 - deriving mean annual pools
+for (y=0;y<noyears;y++){MAPOOL[y]=mean_annual_pool(POOLS,y,n,nopools,deltat);}
+
+// step 2 - checking pool growth
+
+if ((EDC==1 || DIAG==1) && (MAPOOL[noyears-1]/MAPOOL[0]>(1+G*(double)noyears))){EDC=0;EDCD->PASSFAIL[9]=0;}// printf("failed no. 9");}
+
+}
+
+
+
+
+/*EDC no 11*/
+/*POOL EXPONENTIAL DECAY*/
+/*Performed on all seven pools*/
+
+// exporting the decay coefficient here
+/*double C;
+for (n=0;n<nopools;n++)
+{
+    if (EDC==1 || DIAG==1) 
+    {
+        C=expdecay2(POOLS,n,nodays+1,deltat,nopools);
+        if (fabs(-log(2)/C)<365.25*noyears && C<0 && finite(C)){EDC=0;}
+        if (EDC==0){EDCD->PASSFAIL[11-1]=0;}// printf("failed no. 10");}
+    }
+}
+*/
+
+
+
+
+
+//double const fauto=pars[1];
+//double const ffol=(1-fauto)*pars[2];
+//double const flab=(1-fauto-ffol)*pars[12];
+//double const froot=(1-fauto-ffol-flab)*pars[3];
+//double const fwood=1-fauto-ffol-flab-froot;
+
+/*fraction of GPP to som and lit under equilibrium conditions*/
+//double const fsom=fwood+(froot+flab+ffol)*pars[0]/(pars[0]+pars[7]);
+//double const flit=(froot+flab+ffol);
+
+
+
+/*SOM attractor - must be within a factor of 2 from Csom0*/
+/*half the TR is balanced by x2 Csom*/
+
+/*equilibrium factor (in comparison to C_initial)*/
+double EQF=2.0;// JFE - put 2. following chat with AAB
+double meangpp=0;
+for (n=0;n<nodays;n++){meangpp+=FLUXES[n*nofluxes]/(double)nodays;}
+
+
+
+
+/*EDC 12 - SOM steady-state attractor proximity attractor proximity
+if ((EDC==1 || DIAG==1) && meangpp*fsom/(pars[8]*exp(pars[9]*meantemp))>pars[22]*EQF){EDC=0;EDCD->PASSFAIL[12-1]=0;}
+if ((EDC==1 || DIAG==1) && meangpp*fsom/(pars[8]*exp(pars[9]*meantemp))<pars[22]/EQF){EDC=0;EDCD->PASSFAIL[12-1]=0;}
+
+EDC 13 - LIT  -steady-state attractor proximity
+if ((EDC==1 || DIAG==1) && meangpp*flit/(pars[7]*exp(pars[9]*meantemp))>pars[21]*EQF){EDC=0;EDCD->PASSFAIL[13-1]=0;}
+if ((EDC==1 || DIAG==1) && meangpp*flit/(pars[7]*exp(pars[9]*meantemp))<pars[21]/EQF){EDC=0;EDCD->PASSFAIL[13-1]=0;}
+
+EDC 14 - WOO  -steady-state attractor proximity
+if ((EDC==1 || DIAG==1) && meangpp*fwood/(pars[5])>pars[20]*EQF){EDC=0;EDCD->PASSFAIL[14-1]=0;}
+if ((EDC==1 || DIAG==1) && meangpp*fwood/(pars[5])<pars[20]/EQF){EDC=0;EDCD->PASSFAIL[14-1]=0;}
+
+EDC 15 - ROO  -steady-state attractor proximity
+if ((EDC==1 || DIAG==1) && meangpp*froot/(pars[6])>pars[19]*EQF){EDC=0;EDCD->PASSFAIL[15-1]=0;}
+if ((EDC==1 || DIAG==1) && meangpp*froot/(pars[6])<pars[19]/EQF){EDC=0;EDCD->PASSFAIL[15-1]=0;}
+*/
+
+/*Total fluxes*/
+double FT[nofluxes];
+int f=0;
+for (f=0;f<nofluxes;f++){FT[f]=0;for (n=0;n<nodays;n++){FT[f]+=FLUXES[n*nofluxes+f];}}
+
+// replaced expdecay steady-state proximity at t = 0, following PNAS paper
+double Sproxjanmean, Sproxjan;// = malloc(1+nodays/12);
+int yearid; // iterator
+//printf("%d ",noyears);
+
+double Fin[nopools];
+double Fout[nopools];
+
+/*labile*/
+Fin[0]=FT[4];
+Fout[0]=FT[7]+FT[17]+FT[23];
+/*foliar*/
+Fin[1]=FT[3]+FT[7];
+Fout[1]=FT[9]+FT[18]+FT[24];
+/*root*/
+Fin[2]=FT[5];
+Fout[2]=FT[11]+FT[19]+FT[25];
+/*wood*/
+Fin[3]=FT[6];
+Fout[3]=FT[10]+FT[20]+FT[26];
+/*litter*/
+Fin[4]=FT[9]+FT[11]+FT[23]+FT[24]+FT[25];
+Fout[4]=FT[12]+FT[14]+FT[21]+FT[27];
+/*som*/
+Fin[5]=FT[10]+FT[14]+FT[26]+FT[27];
+Fout[5]=FT[13]+FT[22];
+
+int pp = 0; //pool iterator
+/*temporary print switch*/
+int psw=0;
+int nostep_peryear; // get number of time steps per year
+double fin_fout_lim = 0.05; // get the limit of Fin/Fout ratio // JFE added 16/11/16
+
+//printf("nostep_peryear %i ", nostep_peryear);
+//printf("%d",nodays);
+
+int poolid[6] = {0,1,2,3,4,5}; // define pools to check
+int pid=0;
+
+for (pid=0;pid<6;pid++)
+{if ((EDC==1 || DIAG==1) && (fabs(log(Fin[pp]/Fout[pp]))>log(EQF))) {EDC=0;EDCD->PASSFAIL[12-1]=0;}}
+
+// in GSI variable, cfol and clab... sprox not applicable
+for (pid=0;pid>6;pid++)
+{
+    pp = poolid[pid];
+    Sproxjanmean=0;
+    for (yearid=0;yearid<noyears;yearid++)
+    {
+        Sproxjanmean = Sproxjanmean+POOLS[(nostep_peryear*yearid*nopools)+pp];
+    //printf("step: %i ",nostep_peryear*yearid*nopools);
+    }
+    Sproxjanmean = Sproxjanmean/noyears;
+    Sproxjan = (Fin[pp]/Fout[pp])*(Sproxjanmean/POOLS[pp]); //eq. S5
+    
+    //set lower change in soil 
+ //   if (pid == 5) {fin_fout_lim = fin_fout_lim/10.;}
+if ((EDC==1 || DIAG ==1) && (fabs((Fin[pp]/Fout[pp])-Sproxjan) > fin_fout_lim)) {EDC=0;EDCD->PASSFAIL[10]=0;EDCD->PASSFAIL[16+pp]=0;}
+}
+//EDC 16 - average torfol > torwood -  JFE added 26/2/15
+double torfol;
+int nn; 
+
+/* commented out because sum of torfol is already stored in FT
+for (nn=0;n<nodays;nn++){torfol=torfol+FLUXES[nn*nofluxes+28]);}
+torfol=torfol/nodays;
+*/
+torfol = FT[8]/nodays;
+
+if ((EDC==1 || DIAG ==1) && (pars[5]>torfol))
+{EDC=0;EDCD->PASSFAIL[15]=0;}// printf("failed no. 15");}
+
+
+
+
+
+/***********************EDCs done here****************************/
+
+
+
+/*Additional faults can be stored in positions 35-40*/
+
+/*PRIOR RANGES - ALL POOLS MUST CONFORM*/
+
+for (n=0;n<6;n++){if ((EDC==1 || DIAG==1) && ((MPOOLS[n])>PI.parmax[n+17])){EDC=0;EDCD->PASSFAIL[35-1]=0;}}
+
+int PEDC;
+/*ensuring minimum of each pool is zero && finite*/
+if (EDC==1 || DIAG==1)
+{
+    n=0;
+    while (n<nopools && (EDC==1 || DIAG==1))
+    {
+        nn=0;PEDC=1;
+        while (nn<nodays+1 && (PEDC==1))
+        {
+            if (POOLS[n+nn*nopools]<0 || isnan(POOLS[36-1])==1) {EDC=0;PEDC=0;EDCD->PASSFAIL[35+n]=0;}
+            nn=nn+1;
+        }
+        n=n+1;
+    }
+}
+
+
+
+
+
+
+
+
+
+/*FREE MEMORY*/
+free(MPOOLS);
+free(MAPOOL);
+
+
+
+
+
+/*final check confirming EDC = 1 or 0*/
+int Num_EDC=100;
+
+
+if (DIAG==1){for (n=0;n<Num_EDC;n++){if (EDCD->PASSFAIL[n]==0){EDC=0;}}}
+//printf("EDCs: %i",EDC);
+/*Returning EDC */
+return EDC;
+
+
+}
+
