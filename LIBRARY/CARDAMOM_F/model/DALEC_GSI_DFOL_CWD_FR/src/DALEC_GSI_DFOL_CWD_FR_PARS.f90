7bf216c7ada2bb403c5de3f41e18ebaaa978621c
TLS <t.l.smallman@ed.ac.uk>
2018-11-19 16:49:34 +0000

    TLS: Debugging code roll back of new ACM-GPP-ET mk 8

--- LIBRARY/CARDAMOM_F/model/DALEC_GSI_DFOL_CWD_FR/src/DALEC_GSI_DFOL_CWD_FR_PARS.f90
+++ LIBRARY/CARDAMOM_F/model/DALEC_GSI_DFOL_CWD_FR/src/DALEC_GSI_DFOL_CWD_FR_PARS.f90
@@ -9,7 +9,7 @@ module MODEL_PARAMETERS
   public :: pars_info
 
   contains
-  
+
   !
   !------------------------------------------------------------------
   !
@@ -35,69 +35,68 @@ module MODEL_PARAMETERS
        call crop_parameters(PI)
        call crop_development_parameters(PI%stock_seed_labile,PI%DS_shoot &
                                        ,PI%DS_root,PI%fol_frac,PI%stem_frac &
-                                       ,PI%root_frac,PI%DS_LRLV,PI%LRLV & 
+                                       ,PI%root_frac,PI%DS_LRLV,PI%LRLV &
                                        ,PI%DS_LRRT,PI%LRRT)
- 
-    else 
+
+    else
        ! generic model
 
-       ! 
+       !
        ! declare parameters
-       ! 
+       !
 
-       ! Decomposition rate
-       PI%parmin(1)=0.00001
-       PI%parmax(1)=0.01
+       ! Decomposition of litter to som (fraction; temperature adjusted)
+       PI%parmin(1)=0.0001368925d0 ! 20  years at 0oC
+       PI%parmax(1)=0.005475702d0  ! 0.5 years at 0oC
 
        ! Fraction of GPP respired
-       PI%parmin(2)=0.2
-       PI%parmax(2)=0.7
+       PI%parmin(2)=0.2d0
+       PI%parmax(2)=0.7d0
 
        ! GSI sensitivity for leaf growth
-       PI%parmin(3)=1.00
-       PI%parmax(3)=1.02 !1.03 !1.05
+       PI%parmin(3)=1.00d0
+       PI%parmax(3)=1.02d0 !1.03 !1.05
 
        ! Fraction of (1-fgpp) to roots*/
-       PI%parmin(4)=0.01
-       PI%parmax(4)=1.0 ! 0.6 ?
+       PI%parmin(4)=0.01d0
+       PI%parmax(4)=1.0d0 ! 0.6 ?
 
        ! GSI max leaf turnover
-       PI%parmin(5)=0.0001
-       PI%parmax(5)=0.2 ! 0.15 ?
+       PI%parmin(5)=0.00027d0 ! 10 years
+       PI%parmax(5)=0.10d0    ! 10 days
 
-       ! TOR wood* - 1% loss per year value
-       PI%parmin(6)=0.00001
-       PI%parmax(6)=0.001
+       ! Turnover fraction of wood
+       PI%parmin(6)=0.000009d0 ! 300  years
+       PI%parmax(6)=0.001d0    ! 2.73 years
 
-       ! TOR roots
-       PI%parmin(7)=0.00025
-       PI%parmax(7)=0.01
+       ! Turnover fraction of roots
+       PI%parmin(7)=0.0003422313d0 ! 8    years
+       PI%parmax(7)=0.01d0         ! 0.27 years
 
-       ! TOR litter
-       PI%parmin(8)=0.0001
-       PI%parmax(8)=0.01
+       ! Turnover of litter to Rhet (fraction; temperature adjusted)
+       PI%parmin(8)=0.0001368925d0 ! 27 years at 0oC
+       PI%parmax(8)=0.005475702d0  ! 0.50 years at 0oC
 
-       ! TOR SOM
-       PI%parmin(9)=0.000001 ! 0.0000001
-       PI%parmax(9)=0.001 ! 0.0001?
+       ! Turnover of som to Rhet (fraction; temperature adjusted)
+       PI%parmin(9)=1.368925d-6    ! 2000 years at 0oC
+       PI%parmax(9)=0.0002737851d0 ! ~ 10 years at 0oC
 
-       ! Temp factor* = Q10 = 1.2-2.0
-       PI%parmin(10)=0.018
-       PI%parmax(10)=0.07
+       ! Exponential coefficient for Rhet temperature response
+       PI%parmin(10)=0.018d0
+       PI%parmax(10)=0.06d0
 
-       ! log10 avg foliar N (gN.m-2) 
-       ! set to parmin=1 for FLUXCOM only
-       ! e.g. for wetlands etc.
-       PI%parmin(11)=-0.50
-       PI%parmax(11)=0.7
+       ! log10 avg foliar N (gN.m-2)
+       ! Kattge et al., (2011) (Quantiles 2.5% / 97.5%)
+       PI%parmin(11)=-0.2218487d0
+       PI%parmax(11)= 0.5563025d0
 
-       ! GSI max labile turnover
-       PI%parmin(12)=0.0001
-       PI%parmax(12)=0.2 ! 0.15 ?
+       ! Max labile turnover fraction to foliage
+       PI%parmin(12)=0.0006844627d0 ! 4 years 0.0001368925d0 ! 20 years
+       PI%parmax(12)=0.05d0         ! 20 days
 
        ! Fraction to Clab*/
-       PI%parmin(13)=0.05
-       PI%parmax(13)=0.35 
+       PI%parmin(13)=0.05d0
+       PI%parmax(13)=0.35d0
 
        ! GSI min temperature threshold (oC)
        PI%parmin(14)=225d0
@@ -112,9 +111,9 @@ module MODEL_PARAMETERS
        PI%parmax(16)=3600d0*18d0 ! 18 hours
 
        ! LMA
-       ! Kattge et al. 2011, 
-       PI%parmin(17)=10d0 
-       PI%parmax(17)=170d0 !200d0 
+       ! Kattge et al. 2011,
+       PI%parmin(17)=10d0
+       PI%parmax(17)=180d0 !200d0
 
        ! GSI max photoperiod threshold (sec)
        PI%parmin(24)=3600d0*6d0  !  6 hours
@@ -129,32 +128,32 @@ module MODEL_PARAMETERS
        PI%parmax(26)=5500d0
 
        ! critical GPP for LAI increase (fraction)
-       PI%parmin(27)=1e-8
-       PI%parmax(27)=0.20
+       PI%parmin(27)=1d-8
+       PI%parmax(27)=0.20d0
 
        ! fraction of Cwood which is branch
-       PI%parmin(28)=0.05
-       PI%parmax(28)=0.40 !0.65
+       PI%parmin(28)=0.05d0
+       PI%parmax(28)=0.40d0 !0.65
 
        ! fraction of Cwood which is coarse root
-       PI%parmin(29)=0.15
-       PI%parmax(29)=0.30 !0.45
+       PI%parmin(29)=0.15d0
+       PI%parmax(29)=0.30d0 !0.45
 
-       ! GSI senstivity for leaf senescence 
-       PI%parmin(34)=0.97
-       PI%parmax(34)=1.00
+       ! GSI senstivity for leaf senescence
+       PI%parmin(34)=0.97d0
+       PI%parmax(34)=1.00d0
 
        ! GSI - have I just left a growing state (>1)
-       PI%parmin(35)=0.50
-       PI%parmax(35)=1.5
+       PI%parmin(35)=0.50d0
+       PI%parmax(35)=1.5d0
 
-       ! GSI - initial GSI value 
-       PI%parmin(36)=1.0
-       PI%parmax(36)=2.0
+       ! GSI - initial GSI value
+       PI%parmin(36)=1.0d0
+       PI%parmax(36)=2.0d0
 
        ! Turnover rate for CWD
        PI%parmin(38)=0.0001 ! 0.00001
-       PI%parmax(38)=0.01 
+       PI%parmax(38)=1d0/365.25 !0.005  ! 0.01
 
        !
        ! INITIAL VALUES DECLARED HERE
@@ -178,44 +177,44 @@ module MODEL_PARAMETERS
 
        ! C litter
        PI%parmin(22)=1d0
-       PI%parmax(22)=10000d0 ! 5000 ?
+       PI%parmax(22)=5000d0 ?
 
        ! C_som
        PI%parmin(23)=100d0
        PI%parmax(23)=200000d0
- 
+
        ! C CWD
        PI%parmin(37)=1d0
        PI%parmax(37)=10000d0
 
        !
-       ! Replanting pools values 
+       ! Replanting pools values
        !
 
        ! C labile
-       PI%parmin(30)=1.0
-       PI%parmax(30)=100.0
+       PI%parmin(30)=1.0d0
+       PI%parmax(30)=100.0d0
 
        ! C foliar
-       PI%parmin(31)=1.0
-       PI%parmax(31)=100.0
+       PI%parmin(31)=1.0d0
+       PI%parmax(31)=100.0d0
 
        ! C roots
-       PI%parmin(32)=1.0
-       PI%parmax(32)=100.0
+       PI%parmin(32)=1.0d0
+       PI%parmax(32)=100.0d0
 
        ! C_wood derived from forestry yield curves age = 1
-       PI%parmin(33)=1.0
-       PI%parmax(33)=1000.0
+       PI%parmin(33)=1.0d0
+       PI%parmax(33)=1000.0d0
 
-    endif ! crop / default split 
+    endif ! crop / default split
   end subroutine pars_info
   !
   !------------------------------------------------------------------
   !
   subroutine crop_parameters(PI)
 
-    ! Subroutine reads specific parameter ranges for the 
+    ! Subroutine reads specific parameter ranges for the
     ! generic AT_DALEC model
 
     use MCMCOPT, only: parameter_info
@@ -225,115 +224,115 @@ module MODEL_PARAMETERS
     ! declare inputs
     type ( parameter_info ), intent(inout) :: PI
 
-    ! 
+    !
     ! declare parameters
-    ! 
+    !
 
 !    PI%npars=34;
 
     ! Decomposition rate (frac/day)
-    PI%parmin(1)=0.000001*24.0 ; PI%parmax(1)=0.0001*24.0
+    PI%parmin(1)=0.000001d0*24d0 ; PI%parmax(1)=0.0001d0*24d0
 
     ! Fraction of GPP to autotrophic pool
-    PI%parmin(2)=0.2 ; PI%parmax(2)=0.7
+    PI%parmin(2)=0.2d0 ; PI%parmax(2)=0.7d0
 
     ! max development rate (day-1) DS (0->1)
-    PI%parmin(3)=0.030 ; PI%parmax(3)=0.050
+    PI%parmin(3)=0.020d0 ; PI%parmax(3)=0.050d0
     ! max development rate (day-1) DS (1->2)
-    PI%parmin(4)=0.030 ; PI%parmax(4)=0.050
+    PI%parmin(4)=0.010d0 ; PI%parmax(4)=0.050d0
 
     ! turnover rate foliage (frac/day)
-    PI%parmin(5)=1.0e-4*24.0 ; PI%parmax(5)=0.01*24.0
+    PI%parmin(5)=1.0d-4*24d0 ; PI%parmax(5)=0.02d0*24d0
 
     ! TOR wood* - 1% loss per year value (day-1)
-    PI%parmin(6)=1e-4*24.0 ; PI%parmax(6)=0.01*24.0
-    ! maximum rate of foliar turnover (day-1) due to self-shading
-    PI%parmin(7)=1e-5*24.0 ; PI%parmax(7)=0.01*24.0
+    PI%parmin(6)=1d-4*24d0 ; PI%parmax(6)=0.01d0*24d0
+    ! maximum rate of foliar turnover (hr-1) due to self-shading
+    PI%parmin(7)=1d-5*24d0 ; PI%parmax(7)=0.01d0*24d0
 
     ! effective vernalisation days when plants are 50 % vernalised
-    PI%parmin(8)=12.0 ; PI%parmax(8)=32.0
+    PI%parmin(8)=12d0 ; PI%parmax(8)=32d0
 
-    ! mineralisation rate of litter (day-1)
-    PI%parmin(9)=1e-5*24.0 ; PI%parmax(9)=1e-2*24.0
-    ! mineralisation rate of SOM (day-1)
-    PI%parmin(10)=1e-6*24.0 ; PI%parmax(10)=1e-3*24.0
+    ! mineralisation rate of litter (hr-1)
+    PI%parmin(9)=1d-5*24d0 ; PI%parmax(9)=1d-2*24d0
+    ! mineralisation rate of SOM (hr-1)
+    PI%parmin(10)=1d-8*24d0 ; PI%parmax(10)=1d-3*24d0
 
-    ! log10 avg foliar N (gN.m-2) 
+    ! log10 avg foliar N (gN.m-2)
     ! set to parmin=1 for FLUXCOM only
     ! e.g. for wetlands etc.
-    PI%parmin(11)=-0.50 ; PI%parmax(11)=0.7
+    PI%parmin(11)=-0.50d0 ; PI%parmax(11)=0.7d0
 
     ! sow day
-    PI%parmin(12)=115 ; PI%parmax(12)=350
+    PI%parmin(12)=115d0 ; PI%parmax(12)=350d0
 
     ! respiratory cost of labile transfer (per gC.m-2 labile)
-    PI%parmin(13)=0.05 ; PI%parmax(13)=0.4
+    PI%parmin(13)=0.05d0 ; PI%parmax(13)=0.4d0
 
     ! phenological heat units required for emergence
-    PI%parmin(14)=100.0 ; PI%parmax(14)=150.0
+    PI%parmin(14)=100d0 ; PI%parmax(14)=150d0
 
     ! harvest day
-    PI%parmin(15)=15 ; PI%parmax(15)=350
-    ! Plough day
-    PI%parmin(16)=365.25 ; PI%parmax(16)=365.25*4.0
+    PI%parmin(15)=15d0 ; PI%parmax(15)=350d0
+    ! plough day
+    PI%parmin(16)=365.25d0 ; PI%parmax(16)=365.25d0*4d0
 
     ! LMA
-    PI%parmin(17)=10.0 ; PI%parmax(17)=100.0
+    PI%parmin(17)=10d0 ; PI%parmax(17)=100d0
 
-    ! 
+    !
     ! NOTE number order not consistent
     !
 
     ! minimum temperature for development (oC)
-    PI%parmin(26)=(-1.0+273.15) ; PI%parmax(26)=(10.0+273.15)  ! -10,10
+    PI%parmin(26)=(-1d0+273.15d0) ; PI%parmax(26)=(10d0+273.15d0)  ! -10,10
     ! maximum temperature for development (oC)
-    PI%parmin(27)=(10.0+273.15) ; PI%parmax(27)=(36.0+273.15)   ! 20,42
+    PI%parmin(27)=(10d0+273.15d0) ; PI%parmax(27)=(36d0+273.15d0)   ! 20,42
     ! optimum temperature for development (oC)
-    PI%parmin(28)=(10.0+273.15) ; PI%parmax(28)=(30.0+273.15)   ! 10,35
+    PI%parmin(28)=(10d0+273.15d0) ; PI%parmax(28)=(30d0+273.15d0)   ! 10,35
 
     ! minimum temperature for vernalisation (oC)
-    PI%parmin(29)=(-5.3+273.15) ; PI%parmax(29)=(-0.3+273.15)   ! -15,10
+    PI%parmin(29)=(-5.3d0+273.15d0) ; PI%parmax(29)=(-0.3d0+273.15d0)   ! -15,10
     ! maximum temperature for vernalisation (oC)
-    PI%parmin(30)=(12.7+273.15) ; PI%parmax(30)=(18.7+273.15)    ! 5,30
+    PI%parmin(30)=(12.7d0+273.15d0) ; PI%parmax(30)=(18.7d0+273.15d0)    ! 5,30
     ! optimum temperature for vernalisation (oC)
-    PI%parmin(31)=(2.9+273.15) ; PI%parmax(31)=(6.9+273.15)   ! -5,15
+    PI%parmin(31)=(2.9d0+273.15d0) ; PI%parmax(31)=(6.9d0+273.15d0)   ! -5,15
 
-    ! critical photoperiod for development (hrs) 
-    PI%parmin(32)=6.0 ; PI%parmax(32)=12.0
+    ! critical photoperiod for development (hrs)
+    PI%parmin(32)=6d0 ; PI%parmax(32)=13d0
     ! photoperiod sensitivity
-    PI%parmin(33)=0.10 ; PI%parmax(33)=0.35
+    PI%parmin(33)=0.10d0 ; PI%parmax(33)=0.35d0
 
     ! turnover rate of labile
-    PI%parmin(34)=1e-5*24.0  ; PI%parmax(34)=0.00625*24.0
+    PI%parmin(34)=1d-5*24d0  ; PI%parmax(34)=0.00625d0*24d0
     ! turnover rate of autotrophic pool
-    PI%parmin(35)=0.001*24.0 ; PI%parmax(35)=0.07*24.0
+    PI%parmin(35)=0.001d0*24d0 ; PI%parmax(35)=0.07d0*24d0
 
     !
     ! INITIAL VALUES (gC.m-2) DECLARED HERE
     !
 
     ! C labile
-    PI%parmin(18)=1.0 ; PI%parmax(18)=10.0
+    PI%parmin(18)=1d0 ; PI%parmax(18)=10d0
     ! C foliar
-    PI%parmin(19)=1.0 ; PI%parmax(19)=5.0
+    PI%parmin(19)=1d0 ; PI%parmax(19)=5d0
     ! C roots
-    PI%parmin(20)=1.0 ; PI%parmax(20)=5.0
+    PI%parmin(20)=1d0 ; PI%parmax(20)=5d0
     ! C_wood
-    PI%parmin(21)=1.0 ; PI%parmax(21)=5.0
+    PI%parmin(21)=1d0 ; PI%parmax(21)=5d0
     ! C litter
-    PI%parmin(22)=1.0 ; PI%parmax(22)=10
+    PI%parmin(22)=1d0 ; PI%parmax(22)=10d0
     ! C_som
-    PI%parmin(23)=100.0 ; PI%parmax(23)=200000.0
+    PI%parmin(23)=100d0 ; PI%parmax(23)=200000d0
     ! C autotrophic pool
-    PI%parmin(24)=0.1 ; PI%parmax(24)=5.0
+    PI%parmin(24)=0.1d0 ; PI%parmax(24)=5d0
     ! C storage organ
-    PI%parmin(25)=0.1 ; PI%parmax(25)=1.0
+    PI%parmin(25)=0.1d0 ; PI%parmax(25)=1d0
 
   end subroutine crop_parameters
   !
   !--------------------------------------------------------------------------------------------------------------------------------!
   !
-  subroutine crop_development_parameters(stock_seed_labile,DS_shoot,DS_root,fol_frac &                                                 
+  subroutine crop_development_parameters(stock_seed_labile,DS_shoot,DS_root,fol_frac &
                                         ,stem_frac,root_frac,DS_LRLV,LRLV,DS_LRRT,LRRT)
 
     ! subroutine reads in the fixed crop development files which are linked the
@@ -346,12 +345,12 @@ module MODEL_PARAMETERS
     ! crop specific variables
     double precision,intent(inout) :: stock_seed_labile
     double precision, allocatable, dimension(:),intent(inout)  :: DS_shoot, & !
-                                                                   DS_root, & ! 
+                                                                   DS_root, & !
                                                                   fol_frac, & !
                                                                  stem_frac, & !
                                                                  root_frac, & !
                                                                    DS_LRLV, & !
-                                                                      LRLV, & ! 
+                                                                      LRLV, & !
                                                                    DS_LRRT, & !
                                                                       LRRT
 
