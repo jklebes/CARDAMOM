69dc1d58251ad86f64c42fa5a41c91d9f8db97b7
lsmallma <lsmallma@ed.ac.uk>
2022-01-18 09:47:30 +0000

    TLS: Debug of extraction of sand / clay to allow for missing data.

--- R_functions/extract_sand_clay.r
+++ R_functions/extract_sand_clay.r
@@ -21,6 +21,12 @@ extract_sand_clay<- function(spatial_type,resolution,grid_type,latlon_in,sand_cl
   top_clay = sand_clay_all$top_clay[i1,j1]
   bot_clay = sand_clay_all$bot_clay[i1,j1]
 
+  # Guard against NaN values
+  if (is.na(top_sand) | is.infinite(top_sand)) {top_sand = 40}
+  if (is.na(bot_sand) | is.infinite(bot_sand)) {bot_sand = 40}
+  if (is.na(top_clay) | is.infinite(top_clay)) {top_clay = 15}
+  if (is.na(bot_clay) | is.infinite(bot_clay)) {bot_clay = 15}
+
 	# just to check because when averaging sometimes the sand / clay combinations can be > 100 %
 	# 94 % chosesn as this is the highest total % found in the HWSD dataset
 	if ((top_sand+top_clay) > 94) {
