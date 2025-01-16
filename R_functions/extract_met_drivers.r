5796592dcf5483b404da075bef7abba2739e30c7
lsmallma <lsmallma@ed.ac.uk>
2025-01-16 14:28:37 +0000

    TLS: Bug found in the application of a generic grid approach. All DALEC codes require the latitude in WGS84 (-90/90). This was being passed in with as the specified grid type. A sanity check has been added to the R code to ensure that the lat/long is within bounds. A similar check could be added to the DALEC code but not added yet to avoid a delay?

--- R_functions/extract_met_drivers.r
+++ R_functions/extract_met_drivers.r
@@ -30,20 +30,7 @@
 #
 #########################################################################################
 
-extract_met_drivers<-function(n,timestep_days,start_year,end_year,latlon_wanted,met_in,met_source,site_name,grid_type) {
-
-  # Determine local latitude value, ensure it is in wgs-84 -90/90 regardless of grid projection
-  if (grid_type != "epsg:4326") {
-      # The required grid for calculations in this function does not match, 
-      # do the required conversions
-      local_lat = vect(cbind(latlon_wanted[2], latlon_wanted[1]), crs=grid_type) 
-      local_lat = project(local_lat, "epsg:4326")
-      local_lat = crds(local_lat,df=TRUE) # extract latitude, i.e. y-dimension only
-      local_lat = as.vector(local_lat$y)
-  } else {
-      # The required grid is a match for that provided here, assign to local variable and move on
-      local_lat = latlon_wanted[1]
-  } # lat in degrees or not?
+extract_met_drivers<-function(n,timestep_days,start_year,end_year,lat_degrees,met_in,met_source,site_name,grid_type) {
 
   if (met_source == "site_specific") {
 
@@ -164,7 +151,7 @@ extract_met_drivers<-function(n,timestep_days,start_year,end_year,latlon_wanted,
       # rolling averaged for GSI
       avg_days = 30 # assume that the first 30 days are just the actual values, We expect this should result in a small error only
       # create photoperiod information; add 30 days to the output
-      photoperiod_out = calc_photoperiod_sec(local_lat,c(seq(365-(avg_days+1),365,1),doy))
+      photoperiod_out = calc_photoperiod_sec(lat_degrees,c(seq(365-(avg_days+1),365,1),doy))
 
       # now take the daily values and turn them into rolling 30 day averages
       photoperiod_out = rollapply(photoperiod_out,avg_days,mean,na.rm=FALSE)
@@ -200,8 +187,8 @@ extract_met_drivers<-function(n,timestep_days,start_year,end_year,latlon_wanted,
 
       avg_days = 30 # assume that the first 30 days are just the actual values
       # create photoperiod information; add 30 days to the output
-      photoperiod_out = calc_photoperiod_sec(local_lat,c(seq((365-(avg_days-2)),365,1),met_in$doy))
-      
+      photoperiod_out = calc_photoperiod_sec(lat_degrees,c(seq((365-(avg_days-2)),365,1),met_in$doy))
+
       # now take the daily values and turn them into rolling 30 day averages
       photoperiod_out = rollapply(photoperiod_out, avg_days, mean, na.rm=FALSE)
       avgTmax_out = rollapply(avgTmax_out, avg_days, mean, na.rm=FALSE)
