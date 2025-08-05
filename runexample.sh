4d48a3551af2621fb4fe5326bff176b399469ffd
jklebes <jason.klebes@ed.ac.uk>
2025-08-05 11:33:29 +0100

    new input data

--- runexample.sh
+++ runexample.sh
@@ -1,3 +1,3 @@
 rm -r RESULTS/
 mkdir RESULTS/
-LIBRARY/CARDAMOM_F/cardamom ~/Downloads/UK_baseline_sites/UK_baseline_sites/DATA/UK_baseline_sites_AliceHolt.bin ./RESULTS/UK_baseline_sites_AliceHolt_1_  500000 1000 1000 1 0
+LIBRARY/CARDAMOM_F/cardamom ~/Downloads/UK_baseline_sites/UK_baseline_sites_AliceHolt.bin ./RESULTS/UK_baseline_sites_AliceHolt_1_  500000 1000 1000 1 0
